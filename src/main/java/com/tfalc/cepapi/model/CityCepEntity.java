d8955a0ea083f23e6c920280da1133a3cd247d64
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-10-02 22:06:53 -0300

    Update CityCepEntity.java

--- src/main/java/com/tfalc/cepapi/model/CityCepEntity.java
+++ src/main/java/com/tfalc/cepapi/model/CityCepEntity.java
@@ -1,8 +1,6 @@
 package com.tfalc.cepapi.model;
 
-
 import lombok.Data;
-
 import javax.persistence.*;
 
 @Entity
@@ -13,11 +11,12 @@ public class CityCepEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String Cep;
+    private String CityName
 
-    public void FindByCep(final Long id, final String Cep){
+    public void FindByCep(final Long id, final String Cep, final String CityName) {
         this.id = id;
         this.Cep = Cep;
+        this.CityName = CityName;
     }
 }
